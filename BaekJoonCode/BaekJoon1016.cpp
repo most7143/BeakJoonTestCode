f1fea2c6fc07b5788123932386b294750c7dde07
most7143 <most7143@naver.com>
2024-11-07 13:44:55 +0900

    문제 추가

---
+++ BaekJoonCode/BaekJoon1016.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	long min, max;
+
+	cin >> min >> max;
+
+	//min과 max의 길이만큼 vector를 설정
+	vector<bool> check(max - min + 1);
+
+	int count = 0;
+
+	//첫번째 for문은 제곱수만큼 돌아가도록 설정.
+	for (long i = 2; i * i <= max; i++)
+	{
+		//현재 제곱수
+		long pow = i * i;
+
+		//제곱수가 처음 등장하는 위치를 min/pow로 가져온다.
+		//다만, 처음에 등장하는 값이 배수가 아닐 수도 있기 떄문에 아니라면 +1을 해 맞춘다.
+		long startIndex = min / pow;
+
+		if (min % pow != 0)
+		{
+			startIndex++;
+		}
+
+		for (long j = startIndex; pow * j <= max; j++)
+		{
+			//현재 위치 계산. 배열은 0 부터시작하기에 -min을 해줘야 제대로 된 위치를 가져온다.
+			int index = (int)(pow * j) - min;
+
+			check[index] = true;
+		}
+	}
+
+	for (int i = 0; i < check.size(); i++)
+	{
+		if (false == check[i])
+		{
+			count++;
+		}
+	}
+
+	cout << count;
+}
--- BaekJoonCode/BaekJoon1747.cpp
+++ BaekJoonCode/BaekJoon1747.cpp
@@ -1,74 +1,74 @@
-#include<iostream>
-#include<cmath>
-#include<string>
-
-using namespace std;
-
-//뒤집은수가 일치하는 지 체크하는 함수
-bool IsPalin(int a);
-
-int main()
-{
-	const int maxSize = 10000001;
-
-	//배열의 크기가 커 힙으로 할당
-	int* A = new int[maxSize];
-
-	int N;
-	cin >> N;
-
-	for (int i = 2; i < maxSize; i++)
-	{
-		A[i] = i;
-	}
-
-	//소수는 i의 제곱근을 넘지 않는다.
-	for (int i = 2; i < sqrt(maxSize); i++)
-	{
-		if (A[i] == 0)
-		{
-			continue;
-		}
-
-		for (int j = i * 2; j <= maxSize; j += i)
-		{
-			A[j] = 0;
-		}
-	}
-
-	//N보다 크며, 그 중 가장 작은 값부터 탐색
-	for (int i = 0; i < maxSize; i++)
-	{
-		if (A[i] >= N)
-		{
-			if (IsPalin(A[i]))
-			{
-				cout << A[i];
-				break;
-			}
-		}
-	}
-}
-
-bool IsPalin(int a)
-{
-	//비교를 위해 string으로 형변환
-	string str = to_string(a);
-
-	int start = 0;
-	int end = str.size() - 1;
-
-	//start와 end를 비교해 값이 같을때마다 위치를 이동.
-	while (start < end)
-	{
-		if (str[start] != str[end])
-		{
-			return false;
-		}
-
-		start++;
-		end--;
-	}
-
-	return true;
-}
+//#include<iostream>
+//#include<cmath>
+//#include<string>
+//
+//using namespace std;
+//
+////뒤집은수가 일치하는 지 체크하는 함수
+//bool IsPalin(int a);
+//
+//int main()
+//{
+//	const int maxSize = 10000001;
+//
+//	//배열의 크기가 커 힙으로 할당
+//	int* A = new int[maxSize];
+//
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 2; i < maxSize; i++)
+//	{
+//		A[i] = i;
+//	}
+//
+//	//소수는 i의 제곱근을 넘지 않는다.
+//	for (int i = 2; i < sqrt(maxSize); i++)
+//	{
+//		if (A[i] == 0)
+//		{
+//			continue;
+//		}
+//
+//		for (int j = i * 2; j <= maxSize; j += i)
+//		{
+//			A[j] = 0;
+//		}
+//	}
+//
+//	//N보다 크며, 그 중 가장 작은 값부터 탐색
+//	for (int i = 0; i < maxSize; i++)
+//	{
+//		if (A[i] >= N)
+//		{
+//			if (IsPalin(A[i]))
+//			{
+//				cout << A[i];
+//				break;
+//			}
+//		}
+//	}
+//}
+//
+//bool IsPalin(int a)
+//{
+//	//비교를 위해 string으로 형변환
+//	string str = to_string(a);
+//
+//	int start = 0;
+//	int end = str.size() - 1;
+//
+//	//start와 end를 비교해 값이 같을때마다 위치를 이동.
+//	while (start < end)
+//	{
+//		if (str[start] != str[end])
+//		{
+//			return false;
+//		}
+//
+//		start++;
+//		end--;
+//	}
+//
+//	return true;
+//}
