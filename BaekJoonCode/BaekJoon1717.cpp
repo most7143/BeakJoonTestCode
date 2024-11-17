fcfdb44a0e88c496789dd4343e25e35cd11d970e
most7143 <most7143@naver.com>
2024-11-17 15:38:14 +0900

    문제추가

---
+++ BaekJoonCode/BaekJoon1717.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+static vector<int> Parent;
+
+//대표 노드를 찾아 반환하는 함수 (재귀함수)
+int Find(int a)
+{
+	if (a == Parent[a])
+	{
+		return a;
+	}
+	else
+	{
+		return Parent[a] = Find(Parent[a]);
+	}
+}
+
+//대표 노드끼리 연결하는 함수
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	if (a != b)
+	{
+		Parent[b] = a;
+	}
+}
+
+//두 개가 같은 집합인지 확인
+bool CheckSame(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+
+	return a == b;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	Parent.resize(n + 1);
+
+	for (int i = 0; i <= n; i++)
+	{
+		Parent[i] = i;
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int question, a, b;
+
+		cin >> question >> a >> b;
+
+		if (question == 0)
+		{
+			Union(a, b);
+		}
+		else
+		{
+			if (CheckSame(a, b))
+			{
+				cout << "YES" << "\n";
+			}
+			else
+			{
+				cout << "NO" << "\n";
+			}
+		}
+	}
+}
--- BaekJoonCode/BaekJoon2251.cpp
+++ BaekJoonCode/BaekJoon2251.cpp
@@ -1,77 +1,77 @@
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-//물을 옮기는 경우의 수를 미리 배열에 저장 (0,1) , (0,2) , (1,2) ...
-static int Send[] = { 0,0,1,1,2,2 };
-static int Receive[] = { 1,2,0,2,0,1 };
-
-static bool Visited[201][201];
-static bool Answer[201];
-static int Now[3];
-
-void BFS()
-{
-	queue<pair<int, int>> myQueue;
-	myQueue.push(make_pair(0, 0));
-	Visited[0][0] = true;
-	Answer[Now[2]] = true;
-
-	while (false == myQueue.empty())
-	{
-		pair<int, int> p = myQueue.front();
-		myQueue.pop();
-
-		int a = p.first;
-		int b = p.second;
-
-		//3번 용기 = 전체물의 양에서 a,b를 뺀것
-		int c = Now[2] - a - b;
-
-		for (int i = 0; i < 6; i++)
-		{
-			int next[] = { a,b,c };
-			next[Receive[i]] += next[Send[i]];
-			next[Send[i]] = 0;
-
-			//넘치는 양에 대한 처리
-			if (next[Receive[i]] > Now[Receive[i]])
-			{
-				next[Send[i]] = next[Receive[i]] - Now[Receive[i]];
-				next[Receive[i]] = Now[Receive[i]];
-			}
-
-			//방문한 적이 없는 곳에 대한 처리
-			if (false == Visited[next[0]][next[1]])
-			{
-				Visited[next[0]][next[1]] = true;
-				myQueue.push(make_pair(next[0], next[1]));
-
-				if (next[0] == 0)
-				{
-					Answer[next[2]] = true;
-				}
-			}
-		}
-	}
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> Now[0] >> Now[1] >> Now[2];
-
-	BFS();
-
-	for (int i = 0; i < 201; i++)
-	{
-		if (Answer[i])
-		{
-			cout << i << " ";
-		}
-	}
-}
+//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+////물을 옮기는 경우의 수를 미리 배열에 저장 (0,1) , (0,2) , (1,2) ...
+//static int Send[] = { 0,0,1,1,2,2 };
+//static int Receive[] = { 1,2,0,2,0,1 };
+//
+//static bool Visited[201][201];
+//static bool Answer[201];
+//static int Now[3];
+//
+//void BFS()
+//{
+//	queue<pair<int, int>> myQueue;
+//	myQueue.push(make_pair(0, 0));
+//	Visited[0][0] = true;
+//	Answer[Now[2]] = true;
+//
+//	while (false == myQueue.empty())
+//	{
+//		pair<int, int> p = myQueue.front();
+//		myQueue.pop();
+//
+//		int a = p.first;
+//		int b = p.second;
+//
+//		//3번 용기 = 전체물의 양에서 a,b를 뺀것
+//		int c = Now[2] - a - b;
+//
+//		for (int i = 0; i < 6; i++)
+//		{
+//			int next[] = { a,b,c };
+//			next[Receive[i]] += next[Send[i]];
+//			next[Send[i]] = 0;
+//
+//			//넘치는 양에 대한 처리
+//			if (next[Receive[i]] > Now[Receive[i]])
+//			{
+//				next[Send[i]] = next[Receive[i]] - Now[Receive[i]];
+//				next[Receive[i]] = Now[Receive[i]];
+//			}
+//
+//			//방문한 적이 없는 곳에 대한 처리
+//			if (false == Visited[next[0]][next[1]])
+//			{
+//				Visited[next[0]][next[1]] = true;
+//				myQueue.push(make_pair(next[0], next[1]));
+//
+//				if (next[0] == 0)
+//				{
+//					Answer[next[2]] = true;
+//				}
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> Now[0] >> Now[1] >> Now[2];
+//
+//	BFS();
+//
+//	for (int i = 0; i < 201; i++)
+//	{
+//		if (Answer[i])
+//		{
+//			cout << i << " ";
+//		}
+//	}
+//}
