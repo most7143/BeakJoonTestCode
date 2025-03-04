87314bd2c858eae6c74ba867373cb4e9019df84d
most7143 <most7143@naver.com>
2025-03-04 14:47:36 +0900

    백준문제추가

--- BaekJoonCode/BaekJoon11726.cpp
+++ BaekJoonCode/BaekJoon11726.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-
-using namespace std;
-
-int N;
-long D[1001];
-long mod = 10007;
-
-//2×N 크기의 직사각형을 1×2와 2×1 타일을 이용하여 채울 수 있는 경우의 수를 구하는 문제
-
-int main()
-{
-	cin >> N;
-
-	D[1] = 1; //2x1의 경우의 수는 1가지
-	D[2] = 2; //2x2의 경우의 수는 2가지
-
-	for (int i = 3; i <= N; i++)
-	{
-		//DP 점화식
-		D[i] = (D[i - 1] + D[i - 2]) % mod;
-	}
-
-	cout << D[N];
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int N;
+//long D[1001];
+//long mod = 10007;
+//
+////2×N 크기의 직사각형을 1×2와 2×1 타일을 이용하여 채울 수 있는 경우의 수를 구하는 문제
+//
+//int main()
+//{
+//	cin >> N;
+//
+//	D[1] = 1; //2x1의 경우의 수는 1가지
+//	D[2] = 2; //2x2의 경우의 수는 2가지
+//
+//	for (int i = 3; i <= N; i++)
+//	{
+//		//DP 점화식
+//		D[i] = (D[i - 1] + D[i - 2]) % mod;
+//	}
+//
+//	cout << D[N];
+//}
