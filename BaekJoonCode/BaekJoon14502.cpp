851f5506f67d69d51483f63a1ea8c06ee7ac9fb6
most7143 <most7143@naver.com>
2025-04-24 16:03:58 +0900

    백준 코드 추가

---
+++ BaekJoonCode/BaekJoon14502.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+//사방향 이동 배열
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+//처음 맵과, 벽이 세워질 맵을 선언
+int map[8][8];
+int temp[8][8];
+
+int m, n;
+
+int result;
+
+//맵을 카피하는 함수
+void MapCopy(int a[8][8], int b[8][8])
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			a[i][j] = b[i][j];
+		}
+	}
+}
+
+void BFS()
+{
+	queue<pair<int, int>> q;
+
+	// 벽이세워진 상태에서 바이러스를 퍼트릴 배열
+	int spread[8][8];
+
+	//벽 위치 복사
+	MapCopy(spread, temp);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (spread[i][j] == 2)
+			{
+				// 바이러스 위치를 queue에 담기 ( 배열에 있는 모든 바이러스가 동시에 퍼트려야하기 때문)
+				q.push({ i,j });
+			}
+		}
+	}
+
+	while (false == q.empty())
+	{
+		int a = q.front().first;
+		int b = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = a + dx[i];
+			int ny = b + dy[i];
+
+			if (nx >= 0 && ny >= 0 && nx < n && ny < m && spread[nx][ny] == 0)
+			{
+				spread[nx][ny] = 2;
+				q.push({ nx,ny });
+			}
+		}
+	}
+
+	//안전 구역 갯수 저장
+	int cnt = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (spread[i][j] == 0)
+			{
+				cnt++;
+			}
+		}
+	}
+
+	//안전구역 갯수를 비교해 저장
+	result = max(result, cnt);
+}
+
+//벽을 세우는 함수
+void Wall(int wallcnt)
+{
+	//벽이 3개가 세워졌으면 BFS실행
+	if (wallcnt == 3)
+	{
+		BFS();
+		return;
+	}
+	else
+	{
+		//재귀함수 로직, 벽이 배열의 끝까지 도달할 떄 까지 재귀호출
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (temp[i][j] == 0)
+				{
+					temp[i][j] = 1; // 벽을 세우고
+					Wall(wallcnt + 1); // 새로운 벽 생성 , 해당위치에 벽이 생성되었으면 자동적으로 벽이 없는 곳에 배치되어 생성됨
+					temp[i][j] = 0; // 백트랙킹이 되는 부분 . 이전에 생성되었던 벽을 지워줌
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+
+	MapCopy(temp, map);  // 시작할 때 한 번 복사
+	Wall(0);             // 벽 설치 0개부터 시작
+
+	cout << result;
+}
--- BaekJoonCode/BaekJoon7576cpp.cpp
+++ BaekJoonCode/BaekJoon7576cpp.cpp
@@ -1,129 +1,42 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-
-using namespace std;
-
-//범위 설정 - 모든 배열을 순회하진 않기 때문에 최대 길이로 지정
-int boxes[1001][1001];
-
-//상하좌우 탐색로직
-int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,1,-1 };
-
-int m, n;
-
-queue<pair<int, int>> tomatos;
-
-void BFS()
-{
-	while (false == tomatos.empty())
-	{
-		int a = tomatos.front().first;
-		int b = tomatos.front().second;
-
-		tomatos.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = a + dx[i];
-			int ny = b + dy[i];
-
-			//배열 범위를 벗어나지 않으면서, 아직 익지않은 토마토 가 있는지 여부
-			if (nx >= 0 && ny >= 0 && nx < n && ny < m && boxes[nx][ny] == 0)
-			{
-				//몇일 차에 익었는지 해당 배열에 입력
-				boxes[nx][ny] = boxes[a][b] + 1;
-				tomatos.push({ nx,ny });
-			}
-		}
-	}
-}
-
-int main()
-{
-	cin >> m >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> boxes[i][j];
-			//익은 토마토를 quque에 넣는다. 시작점이 여러곳일 수 있기 때문에
-			if (boxes[i][j] == 1)
-			{
-				tomatos.push({ i,j });
-			}
-		}
-	}
-
-	BFS();
-
-	//출력 정하는 곳.
-	int max = 0;
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (boxes[i][j] == 0)
-			{
-				cout << "-1";
-				return 0;
-			}
-
-			//순회하면서 가장 일수가 높은 토마토를 가져온다.
-			if (max < boxes[i][j])
-			{
-				max = boxes[i][j];
-			}
-		}
-	}
-
-	//0이 아닌 1부터 시작했기 때문에 -1 해준다.
-	cout << max - 1;
-
-	return 0;
-}
-
-//시각화용
-
-//void printBoxes() {
-//	cout << "현재 상태:\n";
-//	for (int i = 0; i < n; i++) {
-//		for (int j = 0; j < m; j++) {
-//			cout << boxes[i][j] << " ";
-//		}
-//		cout << '\n';
-//	}
-//	cout << "----------------------\n";
-//}
+//#include<iostream>
+//#include<queue>
+//#include<vector>
+//
+//using namespace std;
+//
+////범위 설정 - 모든 배열을 순회하진 않기 때문에 최대 길이로 지정
+//int boxes[1001][1001];
+//
+////상하좌우 탐색로직
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//
+//int m, n;
+//
+//queue<pair<int, int>> tomatos;
 //
 //void BFS()
 //{
-//	while (!tomatos.empty())
+//	while (false == tomatos.empty())
 //	{
 //		int a = tomatos.front().first;
 //		int b = tomatos.front().second;
 //
 //		tomatos.pop();
 //
-//		cout << "처리 중인 토마토 위치: (" << a << ", " << b << ")\n";
-//
 //		for (int i = 0; i < 4; i++)
 //		{
 //			int nx = a + dx[i];
 //			int ny = b + dy[i];
 //
+//			//배열 범위를 벗어나지 않으면서, 아직 익지않은 토마토 가 있는지 여부
 //			if (nx >= 0 && ny >= 0 && nx < n && ny < m && boxes[nx][ny] == 0)
 //			{
+//				//몇일 차에 익었는지 해당 배열에 입력
 //				boxes[nx][ny] = boxes[a][b] + 1;
 //				tomatos.push({ nx,ny });
-//
-//				cout << "→ 익힘: (" << nx << ", " << ny << ") = " << boxes[nx][ny] << "일차\n";
 //			}
 //		}
-//		printBoxes();
 //	}
 //}
 //
@@ -136,6 +49,7 @@ int main()
 //		for (int j = 0; j < m; j++)
 //		{
 //			cin >> boxes[i][j];
+//			//익은 토마토를 quque에 넣는다. 시작점이 여러곳일 수 있기 때문에
 //			if (boxes[i][j] == 1)
 //			{
 //				tomatos.push({ i,j });
@@ -145,6 +59,7 @@ int main()
 //
 //	BFS();
 //
+//	//출력 정하는 곳.
 //	int max = 0;
 //
 //	for (int i = 0; i < n; i++)
@@ -157,6 +72,7 @@ int main()
 //				return 0;
 //			}
 //
+//			//순회하면서 가장 일수가 높은 토마토를 가져온다.
 //			if (max < boxes[i][j])
 //			{
 //				max = boxes[i][j];
@@ -164,7 +80,91 @@ int main()
 //		}
 //	}
 //
-//	cout << "총 소요일: " << max - 1 << "\n";
+//	//0이 아닌 1부터 시작했기 때문에 -1 해준다.
+//	cout << max - 1;
 //
 //	return 0;
 //}
+//
+////시각화용
+//
+////void printBoxes() {
+////	cout << "현재 상태:\n";
+////	for (int i = 0; i < n; i++) {
+////		for (int j = 0; j < m; j++) {
+////			cout << boxes[i][j] << " ";
+////		}
+////		cout << '\n';
+////	}
+////	cout << "----------------------\n";
+////}
+////
+////void BFS()
+////{
+////	while (!tomatos.empty())
+////	{
+////		int a = tomatos.front().first;
+////		int b = tomatos.front().second;
+////
+////		tomatos.pop();
+////
+////		cout << "처리 중인 토마토 위치: (" << a << ", " << b << ")\n";
+////
+////		for (int i = 0; i < 4; i++)
+////		{
+////			int nx = a + dx[i];
+////			int ny = b + dy[i];
+////
+////			if (nx >= 0 && ny >= 0 && nx < n && ny < m && boxes[nx][ny] == 0)
+////			{
+////				boxes[nx][ny] = boxes[a][b] + 1;
+////				tomatos.push({ nx,ny });
+////
+////				cout << "→ 익힘: (" << nx << ", " << ny << ") = " << boxes[nx][ny] << "일차\n";
+////			}
+////		}
+////		printBoxes();
+////	}
+////}
+////
+////int main()
+////{
+////	cin >> m >> n;
+////
+////	for (int i = 0; i < n; i++)
+////	{
+////		for (int j = 0; j < m; j++)
+////		{
+////			cin >> boxes[i][j];
+////			if (boxes[i][j] == 1)
+////			{
+////				tomatos.push({ i,j });
+////			}
+////		}
+////	}
+////
+////	BFS();
+////
+////	int max = 0;
+////
+////	for (int i = 0; i < n; i++)
+////	{
+////		for (int j = 0; j < m; j++)
+////		{
+////			if (boxes[i][j] == 0)
+////			{
+////				cout << "-1";
+////				return 0;
+////			}
+////
+////			if (max < boxes[i][j])
+////			{
+////				max = boxes[i][j];
+////			}
+////		}
+////	}
+////
+////	cout << "총 소요일: " << max - 1 << "\n";
+////
+////	return 0;
+////}
