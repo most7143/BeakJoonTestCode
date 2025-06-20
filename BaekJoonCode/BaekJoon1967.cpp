f07d68ea425bfe951f0245bd867853c3176ca561
most7143 <most7143@naver.com>
2025-06-20 12:41:25 +0900

    문제 추가

---
+++ BaekJoonCode/BaekJoon1967.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+vector<pair<int, int>> Tree[10001]; //전선 배열 선언
+bool Visited[10001];
+int N, MaxDist, FarNode = 0; //갯수, 최대길이, 가장먼노트
+
+void DFS(int node, int dist)
+{
+	Visited[node] = true;
+
+	//최대 길이가 갱신이 된다면 , 길이와 노드를 재설정
+	if (MaxDist < dist)
+	{
+		MaxDist = dist;
+		FarNode = node;
+	}
+
+	//트리배열을 전부 탐색
+	for (auto next : Tree[node])
+	{
+		int nextNode = next.first;
+		int weight = next.second;
+
+		if (!Visited[nextNode])
+		{
+			DFS(nextNode, dist + weight);
+		}
+	}
+}
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		int parent, child, weight;
+		cin >> parent >> child >> weight;
+
+		//양방향 지정
+		Tree[parent].push_back(make_pair(child, weight));
+		Tree[child].push_back(make_pair(parent, weight));
+	}
+
+	//방문값 초기화 하고 1번지점에서 부터 가장 먼 노드를 탐색
+	memset(Visited, false, sizeof(Visited));
+	DFS(1, 0);
+
+	//방문 값을 초기화하고 FarNode에서 부터 가장 먼 노드를 탐색
+	memset(Visited, false, sizeof(Visited));
+	MaxDist = 0;
+	DFS(FarNode, 0);
+
+	cout << MaxDist;
+}
--- BaekJoonCode/BaekJoon2579.cpp
+++ BaekJoonCode/BaekJoon2579.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int N;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N;
-
-	vector<int> arr(N + 1, 0); // 계단 점수 저장
-	vector<int> dp(N + 1, 0); // 도착한 계단의 누적 점수 저장
-
-	for (int i = 1; i <= N; i++)
-	{
-		cin >> arr[i];
-	}
-
-	dp[1] = arr[1]; // 첫번째 계단은 무조건 밟음
-
-	dp[2] = arr[1] + arr[2]; // 누적 저장
-
-	dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]); // 두가지의 경우의 수 중에서 값을 높은 값 저장
-
-	for (int i = 4; i <= N; i++)
-	{
-		int oneStep = dp[i - 2] + arr[i]; //한칸을 뛰고 도착
-		int twoSteps = dp[i - 3] + arr[i - 1] + arr[i]; // 두 칸 연속 밟기
-
-		dp[i] = max(oneStep, twoSteps);
-	}
-
-	cout << dp[N];
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int N;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> N;
+//
+//	vector<int> arr(N + 1, 0); // 계단 점수 저장
+//	vector<int> dp(N + 1, 0); // 도착한 계단의 누적 점수 저장
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	dp[1] = arr[1]; // 첫번째 계단은 무조건 밟음
+//
+//	dp[2] = arr[1] + arr[2]; // 누적 저장
+//
+//	dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]); // 두가지의 경우의 수 중에서 값을 높은 값 저장
+//
+//	for (int i = 4; i <= N; i++)
+//	{
+//		int oneStep = dp[i - 2] + arr[i]; //한칸을 뛰고 도착
+//		int twoSteps = dp[i - 3] + arr[i - 1] + arr[i]; // 두 칸 연속 밟기
+//
+//		dp[i] = max(oneStep, twoSteps);
+//	}
+//
+//	cout << dp[N];
+//}
