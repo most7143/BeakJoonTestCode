b6eea3070b43f31be7aa8f2636195a14374741ce
most7143 <most7143@naver.com>
2025-06-28 14:51:03 +0900

    백준 문제 추가

---
+++ BaekJoonCode/BaekJoon10816.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <unordered_map>
+//using namespace std;
+//
+//int N, M;
+//
+//unordered_map<int, int> map;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		int value = 0;
+//		cin >> value;
+//
+//		map[value]++;
+//	}
+//
+//	cin >> M;
+//
+//	for (int i = 0; i < M; i++) {
+//		int key = 0;
+//		cin >> key;
+//		cout << map[key] << " ";
+//	}
+//}
