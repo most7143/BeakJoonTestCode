fbff16d3962b630a40af6d4d56da279eca37c8f2
most7143 <most7143@naver.com>
2025-07-03 14:52:44 +0900

    백준 문제 추가

---
+++ BaekJoonCode/BackJoon1806.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int N, S;
+int Arr[100001];
+
+int main()
+{
+	cin >> N >> S;
+
+	int start = 0;
+	int end = 0;
+	int minLength = N + 1;
+	int sum = 0;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> Arr[i];
+	}
+
+	while (true)
+	{
+		//합이 S값보다 크거나 같은 경우 기존에 저장해두었던 최소길이와 비교하고, start 인덱스를 증가시킴(더 짧은 길이가 있는지 체크)
+		if (sum >= S)
+		{
+			minLength = min(minLength, end - start);
+			sum -= Arr[start];
+			start++;
+		}
+		else if (end == N)
+		{
+			break;
+		}
+		else
+		{
+			//합이 S값보다 작은경우 end 인덱스를 증가시킴
+			end++;
+			sum += Arr[end];
+		}
+	}
+
+	if (minLength == N + 1)
+	{
+		cout << 0;
+	}
+	else
+	{
+		//출력시 길이에 +1을 해야함
+		cout << minLength + 1;
+	}
+}
--- BaekJoonCode/BaekJoon15649.cpp
+++ BaekJoonCode/BaekJoon15649.cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-
-using namespace std;
-
-int N, M;
-
-int arr[10];
-bool visited[10];
-
-//depth는 깊이. (x,y) 에서 y를 담당한다.
-void BackTracking(int depth)
-{
-	//depth가 = M 인경우는 y값이 끝까지 도달했을때 이제 그 한줄을 출력한다.
-	if (depth == M)
-	{
-		for (int i = 0; i < M; i++)
-		{
-			cout << arr[i] << " ";
-		}
-
-		cout << "\n";
-
-		return;
-	}
-	else
-	{
-		//백트랙킹 핵심 부분 X의 값을 기준으로 반복을 돌린다
-		for (int i = 1; i <= N; i++)
-		{
-			//사용된 수가 아닐때를 탐색
-			if (false == visited[i])
-			{
-				//방문여부를 갱신하고
-				visited[i] = true;
-
-				//현재 선택된 값을 지정
-				arr[depth] = i;
-
-				//재귀를 통해서 다음 수를 계산.
-				BackTracking(depth + 1);
-
-				//재귀를 빠져나오면 방문여부를 초기화
-				visited[i] = false;
-			}
-		}
-	}
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N >> M;
-	BackTracking(0);
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int N, M;
+//
+//int arr[10];
+//bool visited[10];
+//
+////depth는 깊이. (x,y) 에서 y를 담당한다.
+//void BackTracking(int depth)
+//{
+//	//depth가 = M 인경우는 y값이 끝까지 도달했을때 이제 그 한줄을 출력한다.
+//	if (depth == M)
+//	{
+//		for (int i = 0; i < M; i++)
+//		{
+//			cout << arr[i] << " ";
+//		}
+//
+//		cout << "\n";
+//
+//		return;
+//	}
+//	else
+//	{
+//		//백트랙킹 핵심 부분 X의 값을 기준으로 반복을 돌린다
+//		for (int i = 1; i <= N; i++)
+//		{
+//			//사용된 수가 아닐때를 탐색
+//			if (false == visited[i])
+//			{
+//				//방문여부를 갱신하고
+//				visited[i] = true;
+//
+//				//현재 선택된 값을 지정
+//				arr[depth] = i;
+//
+//				//재귀를 통해서 다음 수를 계산.
+//				BackTracking(depth + 1);
+//
+//				//재귀를 빠져나오면 방문여부를 초기화
+//				visited[i] = false;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> N >> M;
+//	BackTracking(0);
+//}
