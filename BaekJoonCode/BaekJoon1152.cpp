8274cfef1e6e6340f2f8acb4e82262dbfdcd5283
most7143 <most7143@naver.com>
2025-07-08 09:27:47 +0900

    백준 문제 추가

---
+++ BaekJoonCode/BaekJoon1152.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string N;
+	int count = 1;
+
+	getline(cin, N);
+
+	for (int i = 0; i < N.length(); i++)
+	{
+		if (N[i] == ' ')
+		{
+			count++;
+		}
+	}
+
+	if (N[0] == ' ')
+	{
+		count--;
+	}
+
+	if (N[N.length() - 1] == ' ')
+	{
+		count--;
+	}
+
+	cout << count;
+}
