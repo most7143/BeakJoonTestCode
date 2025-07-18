d83c5b31a2c982ec2c9dfd926db7033e2c0ac947
most7143 <most7143@naver.com>
2025-07-18 09:23:57 +0900

    백준 11650 , 1157번 추가

---
+++ BaekJoonCode/BackJoon11650.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<pair<int, int>> Arr;
+
+int N;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+
+		Arr.push_back({ x, y });
+	}
+
+	sort(Arr.begin(), Arr.end());
+
+	for (int i = 0; i < Arr.size(); i++)
+	{
+		cout << Arr[i].first << " " << Arr[i].second << "\n";
+	}
+}
