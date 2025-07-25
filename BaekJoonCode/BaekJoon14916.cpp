9097d0ad3e84d37ae3bf3916befa04a87c6787e1
most7143 <most7143@naver.com>
2025-07-25 12:08:14 +0900

    백준 문제 추가

---
+++ BaekJoonCode/BaekJoon14916.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int result = 0;
+
+int main()
+{
+	cin >> N;
+
+	while (N >= 0)
+	{
+		if (N % 5 == 0)
+		{
+			result += N / 5;
+
+			cout << result;
+			return 0;
+		}
+
+		N -= 2;
+		result++;
+	}
+
+	cout << -1;
+}
